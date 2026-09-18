6acb81590f04e7a883be597fa5f53b33518a99a8
Star Brilliant <coder@poorlab.com>
2026-09-18 03:49:55 +0000

    Fix a “vscode agent host failed to start” error

--- src/vibe/.devcontainer/npm-packages/install.sh
+++ src/vibe/.devcontainer/npm-packages/install.sh
@@ -2,3 +2,6 @@
 
 # We don’t use ghcr.io/devcontainers/features/node to install pnpm because it skips pnpm’s installation scripts on npm v12.
 su "${_REMOTE_USER:-vscode}" -c 'unset HOME && export HOME=~ && umask 0002 && . "${NVM_DIR:-/usr/local/share/nvm}/nvm.sh" && npm install -g --allow-scripts=@pnpm/exe @openai/codex @pnpm/exe @earendil-works/pi-coding-agent'
+
+# Otherwise, VS Code will complain “vscode agent host failed to start” at a later step
+rm -rf /tmp/node-compile-cache
